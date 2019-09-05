36445ab79daa98852b9767bfe57d12077a75e74e
ukik <muhamadduki@gmail.com>
2019-09-05 08:07:02 +0800

    7. JSX Restrictions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm a React App</h1>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <span>only 1 root element in component</span>
+        <br/>
+        <span>naming element attribute cannot same as built-in served word</span>
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));	
+    // return React.createElement('div', {className: 'App'},
+    // React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
