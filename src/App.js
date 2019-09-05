dad6fb5665e7d09cd4cf77643b25dadc817801ac
ukik <muhamadduki@gmail.com>
2019-09-05 14:38:38 +0800

    18. Manipulating the State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
+// Functional Component
+import React, {useState} from 'react';
 import './App.css';
 
 import Person from './Person/Person'
 
+/*
+// class component
+// hanya bisa state biasa
 class App extends Component {
   state = {
     persons: [
@@ -18,22 +22,82 @@ class App extends Component {
       }
     ]
   }
+  ...
+*/
 
-  switchNameHandler = () => {
+// functional component bisa state or useState()
+const app = (props) => {
+  const [
+    personState, // sebagai getters dari persons: []
+    setPersonState, // sebagai setters dari persons: []
+  ] = useState({
+    persons: [
+      {
+        name: "Joni",
+        age: 28
+      }, {
+        name: "Zero",
+        age: 25
+      }, {
+        name: "Megaman",
+        age: 26
+      }
+    ],
+
+    otherState: "some other value, aku hanyalah data untuk memastikan setPersonState tidak mengupdate aku"
+  });
+
+  console.log(personState);
+
+  const switchNameHandler = () => {
     console.log('Was clicked!');
+
+    // DON'T DO THIS: this.state.persons[0].name = "Tony";
+    // dilarang langsung merubah state
+
+    // SOLUSI
+    // replace state menggunakan setters
+    setPersonState({
+      persons: [
+        {
+          name: "Tiara",
+          age: 26
+        }, {
+          name: "Iris",
+          age: 22
+        }, {
+          name: "Angel",
+          age: 24
+        }
+      ]
+    });
+    
   }
 
-  render() {
+  /*
+  Important!
+    ...  
+    render() {
+      return (
+        <DOM>
+      )
+    }
+    ...
+    digunakan pada class component
+  */
+
+  // render() {
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
 
-        {/* 
+        {/*
           Important!
-          - jangan melakukan: 
+          - jangan melakukan:
             ... onClick={this.switchNameHandler()} ...
-            karena method Handler akan dipanggil segera setelah DOM selesai di render 
+            karena method Handler akan dipanggil segera setelah DOM selesai di render
             # kecuali memang kita membutuhkan method Handler tersebut digunakan segera
             # tidak cocok untuk method Handler
             # cocok untuk method init data
@@ -41,7 +105,17 @@ class App extends Component {
             ... onClick={this.switchNameHandler} ...
           - hipotesis: berlaku juga di vue/angular
         */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        {/* 
+          Info!
+          ... onClick={this.switchNameHandler()} ...
+          digunakan pada non var/const function
+
+          ... onClick={this.switchNameHandler()} ...
+          digunakan pada var/const function
+        */}
+
+        <button onClick={switchNameHandler}>Switch Name</button>
 
         <p>
           <span>only 1 root element in component</span>
@@ -57,13 +131,85 @@ class App extends Component {
         */}
 
         {/* dynamically, use state */}
+        <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+        <Person name={personState.persons[1].name} age={personState.persons[1].age}>My Hobbies: Slicing</Person>
+        <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
+
+      </div>
+    );
+
+  // }
+}
+
+export default app;
+
+/*
+// Class Component
+import React, {Component} from 'react'
+import './App.css';
+import Person from './Person/Person'
+
+export default class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Joni",
+        age: 28
+      }, {
+        name: "Zero",
+        age: 25
+      }, {
+        name: "Megaman",
+        age: 26
+      }
+    ],
+    otherState: "some other value, aku hanyalah data untuk memastikan setPersonState tidak mengup" +
+        "date aku"
+  };
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+
+    // DON'T DO THIS: this.state.persons[0].name = "Tony"; dilarang langsung merubah
+    // state
+
+    this.setState({
+      persons: [
+        {
+          name: "Tiara",
+          age: 26
+        }, {
+          name: "Iris",
+          age: 22
+        }, {
+          name: "Angel",
+          age: 24
+        }
+      ]
+
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+
+        <p>
+          <span>only 1 root element in component</span>
+          <br/>
+          <span>naming element attribute cannot same as built-in served word</span>
+        </p>
+
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[0].age}>My Hobbies: Slicing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[0].age}/>
-       
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Slicing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
       </div>
     );
   }
 }
-
-export default App;
+*/
